3632256a3533097fb08f14766301695b9436c60c
aaddiityaa <152051635+aaddiityaa@users.noreply.github.com>
2026-03-04 14:25:37 +0530

    Add Java program to sum two numbers

---
+++ devopp.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int number1 = 10;
+        int number2 = 20;
+        
+        // Declare a variable to store the sum and perform the addition
+        int sum = number1 + number2;
+        
+        // Print the result to the console
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
